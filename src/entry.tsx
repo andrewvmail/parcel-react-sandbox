339d4ae2d506bdbdc35c5d64249d44c7d19ff809
Andrew <andrewvmail@gmail.com>
2022-08-17 11:48:56 -0700

    +

--- src/entry.tsx
+++ src/entry.tsx
@@ -1,6 +1,12 @@
 import * as React from "react";
+import { render } from "react-dom";
+// App wrapped with redux Provider, store, etc.
+
 import { App } from "./App";
-import { createRoot } from "react-dom/client";
-const container = document.getElementById("app");
-const root = createRoot(container);
-root.render(<App />);
+
+render(<App />, document.getElementById("app"));
+
+// import { createRoot } from "react-dom/client";
+// const container = document.getElementById("app");
+// const root = createRoot(container);
+// root.render(<App />);
